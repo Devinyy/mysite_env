e0ed605b44193850088007cfc939aa0b44e6c412
Devin <1010732441@qq.com>
2019-07-16 08:21:02 +0800

    change-html
    change the home's html

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -81,11 +81,22 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+'''
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mysite_db',
+        'USER': 'devin',
+        'PASSWORD': 'zhangxiang123',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
